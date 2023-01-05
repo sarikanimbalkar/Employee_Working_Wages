fd7e84f21f964f501a489327736d7ad080710c57
sarika <sarikanimbalkar97@gmail.com>
2023-01-05 22:57:05 +0530

    Employee_Daily_Wages

--- src/com/bridgelabz/Employee_Attandance_Wages.java
+++ src/com/bridgelabz/Employee_Attandance_Wages.java
@@ -8,18 +8,16 @@ public class Employee_Attandance_Wages {
     public static void main(String[] args) {
         int num = (int) (Math.random() * 10) % 3;
         int dailyWages = 0;
-        switch (num) {
-            case 2:
-                dailyWages = WAGES_PER_HOUR * FULL_DAY_PER_HOUR;
-                System.out.println("Employee is Present...");
-                break;
-            case 1:
-                dailyWages = WAGES_PER_HOUR * PART_TIME_HOUR;
-                System.out.println("Employee is Part Time...");
-                break;
-            default:
-                System.out.println("Employee is Absent...");
+        if (num == 2) {
+            dailyWages = WAGES_PER_HOUR * FULL_DAY_PER_HOUR;
+            System.out.println("Employee is Present...");
+        } else if (num == 1) {
+            dailyWages = WAGES_PER_HOUR * PART_TIME_HOUR;
+            System.out.println("Employee is Part Time...");
         }
+
+        else
+                System.out.println("Employee is Absent...");
             System.out.println("Employees Dayily Wages : " + dailyWages);
         }
     }
