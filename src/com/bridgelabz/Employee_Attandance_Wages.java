a36b83ed624e045d04d2efa2b4382880801c165b
sarika <sarikanimbalkar97@gmail.com>
2023-01-05 23:24:53 +0530

    Employee_Daily_Wages_Working_Per_Hour

--- src/com/bridgelabz/Employee_Attandance_Wages.java
+++ src/com/bridgelabz/Employee_Attandance_Wages.java
@@ -3,12 +3,15 @@ package com.bridgelabz;
 public class Employee_Attandance_Wages {
     static final int WAGES_PER_HOUR = 20;
     static final int FULL_DAY_PER_HOUR = 8;
-    static final int PART_TIME_HOUR=4;
-    static final int WORKING_DAY_MONTH=20;
-static int totalWages=0;
+    static final int PART_TIME_HOUR = 4;
+    static final int WORKING_DAY_MONTH = 20;
+    static final int WORKING_HOUR_PER_MONTH = 100;
+    static int totalWages = 0;
+
     public static void main(String[] args) {
         int checkCount = 0;
-        while (checkCount <= WORKING_DAY_MONTH) {
+        int workingHours = 0;
+        while (checkCount <= WORKING_DAY_MONTH && workingHours < WORKING_HOUR_PER_MONTH) {
             int num = (int) (Math.random() * 10) % 3;
             int dailyWages = 0;
             switch (num) {
@@ -23,10 +26,10 @@ static int totalWages=0;
                 default:
                     System.out.println("Employee is Absent...");
             }
-            totalWages +=dailyWages;
+            totalWages += dailyWages;
             System.out.println("Employees Dayily Wages : " + dailyWages);
             System.out.println("Employee Total Wages is : " + totalWages);
             checkCount++;
         }
     }
-    }
+}
