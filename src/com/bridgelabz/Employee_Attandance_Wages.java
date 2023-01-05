50103bbc0d716e03181633d4061bdf82662130b0
sarika <sarikanimbalkar97@gmail.com>
2023-01-05 21:59:48 +0530

    Employee_Check_Present

--- src/com/bridgelabz/Employee_Attandance_Wages.java
+++ src/com/bridgelabz/Employee_Attandance_Wages.java
@@ -1,15 +1,17 @@
 package com.bridgelabz;
 
 public class Employee_Attandance_Wages {
-    public static void main(String[] args) {
-        int num=(int) (Math.random()*10) %2;
-        System.out.println(num);
-        if (num==1)
-        {
-            System.out.println("Employee is Present...");
-        }
-        else
-            System.out.println("Employee is not Present...");
+    static final int WAGES_PER_HOUR = 20;
+    static final int FULL_DAY_PER_HOUR = 8;
 
+    public static void main(String[] args) {
+        int num = (int) (Math.random() * 10) % 2;
+        int dailyWages=0;
+        if (num == 1) {
+            dailyWages=WAGES_PER_HOUR*FULL_DAY_PER_HOUR;
+            System.out.println("Employee is Present...");
+        } else
+            System.out.println("Employee is not Present...");
+        System.out.println("Employees Dayily Wages : " + dailyWages);
     }
 }
